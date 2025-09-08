309bd63e94ad1f67e2636d558c7040f0339b5a3c
Dennis0101 <gamehack1346@gmail.com>
2025-09-08 15:47:09 +0900

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{ts,tsx,js,jsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      container: { center: true, padding: "1rem" }
+    },
+  },
+  plugins: [],
+};
