01bfe5e07a2921e8dac275d97821f62b67807126
Dennis0101 <gamehack1346@gmail.com>
2025-09-08 14:00:54 +0900

    Update route.ts

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,6 +1,6 @@
 // src/app/api/auth/register/route.ts
 import { NextRequest, NextResponse } from "next/server";
-import prisma from "@/lib/prisma";
+import prisma from "../../../../lib/prisma";
 import { hash } from "bcrypt";
 import { z } from "zod";
 
