313933c716bf737cd81688376c5075a9c233825e
Dennis0101 <gamehack1346@gmail.com>
2025-09-08 13:06:42 +0900

    Create page.tsx

---
+++ src/app/(dashboard)/profile/page.tsx
@@ -0,0 +1,8 @@
+export default function ProfilePage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-semibold mb-4">내 프로필</h1>
+      <p>사용자 정보, 비밀번호 변경 등이 들어갑니다.</p>
+    </div>
+  );
+}
